e36ead8241ab056bebb96552f8d5f198c9332601
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2024-08-09 10:26:21 +0100

    control - change only logger info

--- index.js
+++ index.js
@@ -72,8 +72,11 @@ const resolver = Consumer.create({
         requestType = 'GET';
         const getCaseResponse = await getCase.fetch();
         const isCaseFound = (getCaseResponse.exists ? 'found' : 'not found');
-        logger.info({ externalID, message: 'Casework GET request successful. externalId:' +
-          `${externalID} was ${isCaseFound}`});
+        logger.info({
+          message: `Casework GET request successful. External ID: ${externalID} was ${isCaseFound}`,
+          externalID: externalID,
+          status: isCaseFound
+        });
         caseID = getCaseResponse.caseId;
 
         if (!getCaseResponse.exists) {
@@ -92,12 +95,46 @@ const resolver = Consumer.create({
         await submitAudit('duplicates', { caseID, externalID });
         await submitAudit('resolver', { success: true, caseID, externalID });
         return resolve();
-      } catch (e) {
+      } 
+      catch (e) {
         return handleError(caseID, externalID, requestType, reject, e);
       }
     });
   }
 });
+
+// try {
+      //   requestType = 'GET';
+      //   const getCaseResponse = await getCase.fetch();
+      //   const isCaseFound = (getCaseResponse.exists ? 'found' : 'not found');
+      //   logger.info({
+      //     message: `Casework GET request successful. External ID: ${externalID} was ${isCaseFound}`,
+      //     externalID: externalID,
+      //     status: isCaseFound
+      //   });
+      //   caseID = getCaseResponse.caseId;
+
+      //   if (!getCaseResponse.exists) {
+      //     requestType = 'CREATECASE';
+      //     const data = await submitCase.save();
+      //     const { caseid: caseId } = data?.data?.createcaseresponse || {};
+
+      //     if (!caseId) {
+      //       logger.warn({ message: 'Failed to extract Case ID', data });
+      //     }
+
+      //     logger.info({ caseId, externalID, message: 'Casework submission successful' });
+
+      //     await submitAudit('resolver', { success: true, caseId, externalID });
+      //     return resolve();
+      //   }
+
+      //   logger.info({ externalID, message: `Case already submitted with iCasework Case ID ${caseID}` });
+
+      //   await submitAudit('duplicates', { caseID, externalID });
+      //   await submitAudit('resolver', { success: true, caseID, externalID });
+      //   return resolve();
+      // }
 
 resolver.on('error', err => {
   console.error(err.message);
