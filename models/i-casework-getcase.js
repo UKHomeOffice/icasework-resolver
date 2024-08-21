65df99ea96d94ed2d2fedb32c5a899519ada4ca1
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2024-08-21 15:59:33 +0100

    logs

--- models/i-casework-getcase.js
+++ models/i-casework-getcase.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const Model = require('hof').model;
+// const { model: Model } = require('hof');
+const Model = require('./i-casework');
 const crypto = require('crypto');
 const config = require('../config');
-const axios = require('axios');
+const logger = require('hof/lib/logger')({ env: config.env });
+// const axios = require('axios');
 
 module.exports = class DocumentModel extends Model {
   constructor(attributes, options) {
@@ -13,7 +15,7 @@ module.exports = class DocumentModel extends Model {
 
   url() {
     // we are just building up the url with the path
-    return `${config.icasework.url}${config.icasework.getcasepath}?db=${config.icasework.db}`;
+    return `${config.icasework.url}${config.icasework.getcasepath}`;
   }
 
   sign() {
@@ -21,15 +23,22 @@ module.exports = class DocumentModel extends Model {
     return crypto.createHash('md5').update(this.get('ExternalId') + date + config.icasework.secret).digest('hex');
   }
 
+  // prepare() {
+  //   const params = {
+  //     db: config.icasework.db,
+  //     Key: config.icasework.key,
+  //     Signature: this.sign(),
+  //     ExternalId: this.get('ExternalId'),
+  //     Format: 'json'
+  //   };
+  //   return params;
+  // }
+
   prepare() {
-    const params = {
-      // db: config.icasework.db,
-      Key: config.icasework.key,
-      Signature: this.sign(),
-      ExternalId: this.get('ExternalId'),
-      Format: 'json'
-    };
-    return params;
+    const props = super.prepare();
+    props.ExternalId = this.get('ExternalId');
+    console.log(props);
+    return props;
   }
 
   handleResponse(response, callback) {
@@ -50,50 +59,57 @@ module.exports = class DocumentModel extends Model {
     }
   }
 
-  async fetch() {
+  // async fetch() {
+  //   let params = {
+  //     // url: this.url(),
+  //     // method: 'get',
+  //     params: this.prepare()
+  //   };
+
+  //   await axios.get(this.url(), params)
+  //     .then(response => {
+  //       console.log('response with  url and params: ', response);
+  //     })
+  //     .catch(error => {
+  //       // eslint-disable-next-line no-console
+  //       console.error(' url  and params error: ', error);
+  //       // throw error;
+  //     });
+  //   try {
+  //     // const options = this.requestConfig({});
+  //     // options.url = this.url();
+  //     // options.params = this.prepare();
+  //     // options.method = 'GET';
+  //     params = {
+  //       url: this.url(),
+  //       method: 'GET',
+  //       params: this.prepare()
+  //     };
+  //     // console.log('******************* THIS IS BEFORE THE FETCH RESPONSE: ', options);
+  //     console.log('******************* THIS IS THE FETCH RESPONSE: ', await this.request(params));
+  //     console.log('******************* THIS IS THE private FETCH RESPONSE: ', await this._request(params));
+  //     // const response = await this._request(options);
+  //     // console.log('******************* THIS IS HANDLING RESPONSE: ', this.handleResponse(response));
+  //     return await this._request({ params });
+  //     // return this.handleResponse(response.data);
+  //   } catch (err) {
+  //     console.error('Error fetching data:', err);
+  //     throw err;
+  //   }
+  // }
+
+  fetch() {
     const params = {
       url: this.url(),
-      method: 'get',
+      method: 'GET',
       params: this.prepare()
     };
-    await axios(params)
-      .then(response => {
-        console.log('response with params: ', response);
-      })
-      .catch(error => {
-        // eslint-disable-next-line no-console
-        console.error('with params error: ', error);
-        throw error;
+    return this._request(params).then(response => {
+      return console.log(response);
+    })
+      .catch(err => {
+        logger.error(`Error fetching data from ${params.url}: ${err.message}`);
+        throw new Error(`Failed to fetch data: ${err.message || 'Unknown error'}`);
       });
-    await axios(this.url(), { params })
-      .then(response => {
-        console.log('response with  url and params: ', response);
-      })
-      .catch(error => {
-        // eslint-disable-next-line no-console
-        console.error(' url  and params error: ', error);
-        throw error;
-      });
-    // try {
-    //   const options = this.requestConfig({});
-    //   options.url = this.url();
-    //   options.params = this.prepare();
-    //   options.method = 'GET';
-    //   // const params = {
-    //   //   url: this.url(),
-    //   //   method: 'GET',
-    //   //   params: this.prepare()
-    //   // };
-    //   console.log('******************* THIS IS BEFORE THE FETCH RESPONSE: ', options);
-    //   console.log('******************* THIS IS THE FETCH RESPONSE: ', await this.request(options));
-    //   console.log('******************* THIS IS THE private FETCH RESPONSE: ', await this._request(options));
-    //   const response = await this._request(options);
-    //   console.log('******************* THIS IS HANDLING RESPONSE: ', this.handleResponse(response));
-    //   return await this._request(options);
-    //   // return this.handleResponse(response.data);
-    // } catch (err) {
-    //   console.error('Error fetching data:', err);
-    //   throw err;
-    // }
   }
 };
