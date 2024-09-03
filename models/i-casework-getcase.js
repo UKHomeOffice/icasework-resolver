59d41b73c1f0954fd10aca3f25e58664ebbbd134
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2024-09-03 15:16:04 +0100

    test with axios

--- models/i-casework-getcase.js
+++ models/i-casework-getcase.js
@@ -55,11 +55,9 @@ module.exports = class DocumentModel extends Model {
     console.log('******************* THIS IS BEFORE THE CONTROL FETCH RESPONSE: ', await this.request(options));
     const response =  await this.request(options);
     console.log('******************* THIS IS AFTER THE CONTROL FETCH RESPONSE: ', response);
-    console.log('************************Simple get with axios ', await axios.get(config.icasework.url));
-    console.log('************************Fetching with axios ', await axios.get(this.url(), this.prepare()));
-    const getResponse = await axios.get(this.url());
-    console.log('************************Simple get with axios 2 ', getResponse);
-    console.log('************************Fetching with axios ', await axios.get(this.url(), this.prepare()));
+    console.log('HELLLLLLLOOOOOOOOOO')
+    console.log('************************Fetching with axios ', await axios.get(this.url(), options));
+    console.log('************************Fetching with axios 2 ', await axios.get(this.url(), this.prepare()));
     const fetchResponse = await axios.get(this.url(), this.prepare());
     console.log('************************Fetching with axios ', fetchResponse);
 
